c8653185f1d98c748fd1762b500d2ff6f952f287
Miroslav Genov <mgenov@gmail.com>
2020-10-18 10:53:49 +0300

    build: update docker library deps

--- src/main/kotlin/com/clouway/testing/firestore/GoogleCloudContainer.kt
+++ src/main/kotlin/com/clouway/testing/firestore/GoogleCloudContainer.kt
@@ -1,10 +1,11 @@
 package com.clouway.testing.firestore
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
 
 /**
  * A testing container that starts firestore emulator.
  *
  * @author Miroslav Genov (miroslav.genov@clouway.com)
  */
-class GoogleCloudContainer : GenericContainer<GoogleCloudContainer>("clouway/firestore-emulator:alpine")
+class GoogleCloudContainer : GenericContainer<GoogleCloudContainer>(DockerImageName.parse("clouway/firestore-emulator:alpine"))
